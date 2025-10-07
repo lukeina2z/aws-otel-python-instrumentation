4b4734c6c4cfa16997bfd92e8cc19550edd65382
Luke Zhang <lukezha@amazon.com>
2025-10-07 12:51:04 -0700

    enable adot with mcp server.

--- sample-applications/mcp-client-server/python-mcp-server-foo/zrun.sh
+++ sample-applications/mcp-client-server/python-mcp-server-foo/zrun.sh
@@ -12,10 +12,11 @@ OTEL_LOGS_EXPORTER=none \
 OTEL_AWS_APPLICATION_SIGNALS_ENABLED=true \
 OTEL_PYTHON_DISTRO=aws_distro \
 OTEL_PYTHON_CONFIGURATOR=aws_configurator \
-OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
+# OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
 OTEL_TRACES_SAMPLER=xray \
 OTEL_TRACES_SAMPLER_ARG="endpoint=http://localhost:2000" \
 OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT=http://localhost:4316/v1/metrics \
-OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces \
-OTEL_RESOURCE_ATTRIBUTES="service.name=mcp-server-foo-otelTest" \
-opentelemetry-instrument python ./oteltest.py
+# OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces \
+OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://xyz-jaeger-100:4317/v1/traces \
+OTEL_RESOURCE_ATTRIBUTES="service.name=mcp-server-foo-zrun33" \
+opentelemetry-instrument python ./mcp_simple_tool/server.py
