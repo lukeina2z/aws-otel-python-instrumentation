ad7395b61d5b243d33b674b0e667e529503a3421
Luke Zhang <lukezha@amazon.com>
2025-10-24 20:37:48 -0700

    to 1.37.0 , 0.58b0

---
+++ zRun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+pip freeze | grep -v "@" | xargs pip uninstall -y
+pip uninstall -y aws-opentelemetry-distro
+pip uninstall -y contract-tests 
+pip uninstall -y mock-collector 
+ 
+
+# docker network ls | grep aws-application-signals-network
+docker network rm aws-application-signals-network
+docker network prune -f
+
+
+./scripts/build_and_install_distro.sh
+
+./scripts/set-up-contract-tests.sh
+
+pytest contract-tests/tests
+
